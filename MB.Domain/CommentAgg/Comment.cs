6596050bbab5fcce54a8df8ae970d5b045c85079
Rezaei-ali <ali1998rezaei3@yahoo.com>
2023-12-29 17:39:49 +0330

    Comment Domain Implemented.

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,29 @@
+﻿namespace MB.Domain.CommentAgg;
+
+public class Comment
+{
+    public long Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Message { get; private set; }
+    public int Status { get; private set; }
+    public string CreationDate { get; private set; }
+    public long ArticleId { get; private set; }
+    public Article.Article Article { get; set; }
+
+    protected Comment()
+    {
+        
+    }
+    
+    public Comment(string name, string email, string message,  long articleId)
+    {
+        Name = name;
+        Email = email;
+        Message = message;
+        CreationDate = DateTime.Now.ToString();
+        ArticleId = articleId;
+        Status = Statuses.New;
+    }
+
+}
